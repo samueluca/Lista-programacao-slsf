af05c8312f202628f6b54d6e44cae558b83faf66
samuelucas <samuelucas708@gmail.com>
2025-08-25 22:00:03 -0300

    Atividades em Sala

--- AtvidadeEmSala/src/AtividadeSala13.java
+++ AtvidadeEmSala/src/AtividadeSala13.java
@@ -3,13 +3,32 @@ import java.util.Scanner;
 public class AtividadeSala13 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        double[] [] boletim = {
-        {0,0,0},
-        {0,0,0},
-        {0,0,0}
+        double[][] boletim = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
 
         };
 
-        String[] materias = {"Matemática","Português,","Física"};
+
+        String[] materias = {"Matemática", "Português,", "Física"};
+
+        for (int linha = 0; linha < 3; linha++) {
+            System.out.println("Digite as nootas do trimestre de " + materias[linha]);
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.println("Digite a nota do" + (coluna + 1) + "trimestre");
+                boletim[linha][coluna] = leia.nextDouble();
+            }
+        }
+        System.out.println("Boletim:");
+        for (int linha = 0; linha < 3; linha++) {
+            System.out.println(materias[linha]);
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.println((coluna + 1) + " etapa: "
+                        + boletim[linha][coluna] + "pontos");
+            }
+        }
+
+
     }
 }
---
+++ AtvidadeEmSala/src/AtividadeSala14.java
@@ -0,0 +1,20 @@
+public class AtividadeSala14 {
+
+    public static int soma(int x, int y){
+        return  x + y;
+
+    }
+    public static void imprimir(int valor){
+        System.out.println(valor);
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(soma(20, 35));
+
+
+    }
+}

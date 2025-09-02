a361030d77872748d2890f7dc4ed4cdd820d7889
samuelucas <samuelucas708@gmail.com>
2025-09-01 21:49:44 -0300

    Atividade 36 a 40

--- AtividadesJava/src/Atividade35.java
+++ AtividadesJava/src/Atividade35.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
 
-public class Atividade35{
+public class Atividade35 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        int num, maior = Integer.MIN_VALUE;
+        int numero;
+        int maior = 0;
+        boolean primeiroNumero = true;
 
-        System.out.println("Digite numeros inteiros (-1 para parar):");
-        do {
-            num = leia.nextInt();
-            if (num != -1 && num > maior) maior = num;
-        } while (num != -1);
+        while (true) {
+            System.out.print("Digite um número inteiro (-1 para encerrar): ");
+            numero = leia.nextInt();
 
-        if (maior != Integer.MIN_VALUE) System.out.println("Maior numero: " + maior);
+            if (numero == -1) {
+                break;
+            }
+
+            if (primeiroNumero) {
+                maior = numero;
+                primeiroNumero = false;
+            } else if (numero > maior) {
+                maior = numero;
+            }
+        }
+
+        if (primeiroNumero) {
+            System.out.println("Nenhum número foi digitado.");
+        } else {
+            System.out.println("O maior número digitado foi: " + maior);
+        }
     }
 }
